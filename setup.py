5a861a0ee92f867e25e72471e6f80d48503ab785
dervism <dervis4@gmail.com>
2020-06-11 13:09:52 +0200

    Adding logging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xlnet',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='dervism',
+    author_email='',
+    description='NLP Sentiment Analysis test'
+)
--- xlnet.py
+++ xlnet.py
@@ -1,13 +1,15 @@
-from transformers import pipeline
 import logging
 
+from transformers import pipeline
+
+# logging config doc: https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
+logging.basicConfig(level="WARN", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 def main():
-    # Allocate a pipeline for sentiment-analysis
     nlp = pipeline("sentiment-analysis")
     result = nlp("We are very happy to include pipeline into the transformers repository.")
-    print(result);
+    logger.warning("Result: %s %s", result[0]['label'], result[0]['score'])
 
 if __name__ == "__main__":
     main()
