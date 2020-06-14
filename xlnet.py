496d02d798ef3e8aa9bb92cd94707fcf5a445a3c
dervism <dervis4@gmail.com>
2020-06-14 18:10:44 +0200

    Refactor and add fine-tuned models for multi-class classification.

--- xlnet.py
+++ xlnet.py
@@ -1,50 +1,73 @@
-import argparse
-
+import tensorflow
 import torch
+import transformers
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import TextClassificationPipeline
+from transformers import XLNetTokenizer, XLNetForSequenceClassification
 from transformers import pipeline
+
+
+def readData(filename, f):
+    with open(filename) as file:
+        for line in file: f(line)
 
 
 def setupDefaultSentimentAnalysis() -> TextClassificationPipeline:
     return pipeline(task="sentiment-analysis")
 
 
-def setupSentimentAnalysis(modelName):
+def setupXLNetSentimentAnalysis(modelName):
+    tokenizer = XLNetTokenizer.from_pretrained(modelName)
+    model = XLNetForSequenceClassification.from_pretrained(modelName)
+    return pipeline(task="sentiment-analysis", model=model, tokenizer=tokenizer)
+
+
+def setupBertSentimentAnalysis(modelName):
+    tokenizer = BertTokenizer.from_pretrained(modelName)
+    model = BertForSequenceClassification.from_pretrained(modelName)
+    return pipeline(task="sentiment-analysis", model=model, tokenizer=tokenizer)
+
+def setupAuto(modelName):
     tokenizer = AutoTokenizer.from_pretrained(modelName)
     model = AutoModelForSequenceClassification.from_pretrained(modelName)
-    return (tokenizer, model, pipeline(task="sentiment-analysis", model=model, tokenizer=tokenizer))
+    return pipeline(task="sentiment-analysis", model=model, tokenizer=tokenizer)
 
+def setup(model="default pipline"):
+    print(model, ':')
+    return {
+        "default": lambda: setupAuto("distilbert-base-uncased-finetuned-sst-2-english"),
+        "xlnet": lambda: setupXLNetSentimentAnalysis("textattack/xlnet-large-cased-SST-2"),
+        "bert": lambda: setupBertSentimentAnalysis("bert-base-uncased"),
+        "imdb": lambda: setupAuto("lvwerra/bert-imdb"),
+        "finbert": lambda: setupAuto("ipuneetrathore/bert-base-cased-finetuned-finBERT")
+    }.get(model, lambda: setupDefaultSentimentAnalysis())()
 
 def main():
-    parser = argparse.ArgumentParser(description='XLNet Language Model')
-    parser.add_argument('--model', type=str, default='xlnet')
-    args = parser.parse_args()
 
-    negativeStr = "This film is terrible"
-    positiveStr = "This film is great"
+    def newline(): print()
+    def result(f, j, s:str): f("Result: ", j(s))
 
-    _, _, nlp = setupSentimentAnalysis("bert-base-cased")
-    print("bert-base-cased:")
-    print("Result: %s", nlp(negativeStr))
-    print("Result: %s", nlp(positiveStr))
-    print()
+    negativeStr = "This film is terrible."
+    positiveStr = "This film is great."
+    neutralStr = "I saw the film."
 
-    tokenizer, model, nlp = setupSentimentAnalysis("textattack/xlnet-large-cased-STS-B")
-    print("textattack/xlnet-large-cased-STS-B:")
-    print("Result: %s", nlp(negativeStr))
-    print("Result: %s", nlp(positiveStr))
-    print()
+    print("Versions:")
+    print("Torch ", torch.__version__)
+    print("Tensorflow ", tensorflow.__version__)
+    print("Transformers ", transformers.__version__)
+    newline()
 
-    # Example is from https://huggingface.co/transformers/model_doc/xlnet.html#xlnetforsequenceclassification
-    print("Calling the model directly (HuggingFace example from the documentation)")
-    input_ids = torch.tensor(tokenizer.encode(negativeStr, add_special_tokens=True)).unsqueeze(0)
-    outputs = model(input_ids, labels=torch.tensor([1]).unsqueeze(0))
-    print("Result %s", outputs)
+    models = [
+        "default", "bert", "imdb", "finbert", "xlnet"
+    ]
 
-    input_ids = torch.tensor(tokenizer.encode(positiveStr, add_special_tokens=True)).unsqueeze(0)
-    outputs = model(input_ids, labels=torch.tensor([1]).unsqueeze(0))
-    print("Result %s", outputs)
+    for model in models:
+        nlp = setup(model)
+        result(print, nlp, negativeStr)
+        result(print, nlp, positiveStr)
+        result(print, nlp, neutralStr)
+        newline()
 
 
 if __name__ == "__main__":
